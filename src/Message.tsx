7a197d6ce28c2b90ea860b59920c3badbd4a948c
Dana Benish <danabenish@eduroam-226-87-32.laptops.usf.edu>
2025-01-13 15:04:20 -0500

    Updates

--- src/Message.tsx
+++
@@ -1,7 +0,0 @@
-function Message(){ // Child
-    //JavaScript XML - compiled to JavaScript not HTML (convertrd)
-    const name = 'Dana';
-    return <h1>Hello World {name}</h1>
-}
-
-export default Message;
